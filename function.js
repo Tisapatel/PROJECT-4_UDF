ae09afbd714982b4986051d6b42439ad399257ec
Tisa <trpatel.sd21@gmail.com>
2025-05-29 15:55:11 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,22 @@
+// 1. First Type
+function printMsg(){
+    console.log('Hello World!');
+    
+}
+
+printMsg();
+
+// 2. Second Type
+let logMsg = function(){
+    console.log("Hello JS Devlopers...");
+    
+}
+
+logMsg();
+
+// 3. ArrowFunction Type
+
+let Arrowfunc = ()=>{
+    console.log("Hello FS Devlopers...");
+    
+}
